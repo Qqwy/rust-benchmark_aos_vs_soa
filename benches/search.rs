02d2374b57ca72c7161f385e4191a7ae7f94d04d
qqwy <qqwy@gmx.com>
2023-02-24 13:41:44 +0100

    Use iter_batched instead

--- benches/search.rs
+++ benches/search.rs
@@ -1,4 +1,4 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion, BenchmarkId};
+use criterion::{black_box, criterion_group, criterion_main, Criterion, BenchmarkId, BatchSize};
 use quickcheck::{Gen, Arbitrary};
 
 use soa_bench::*;
@@ -18,32 +18,29 @@ pub fn criterion_benchmark(c: &mut Criterion) {
 
         let mut group = c.benchmark_group("search");
         group.bench_with_input("aos", &size, |b, _size| {
-            b.iter(|| {
-                let (lower_needle, higher_needle) = gen_needles(&mut gen);
+            b.iter_batched(||gen_needles(&mut gen),|(lower_needle, higher_needle)| {
                 let lower_needle = (&lower_needle.0, &lower_needle.1, &lower_needle.2, &lower_needle.3, &lower_needle.4);
                 let higher_needle = (&higher_needle.0, &higher_needle.1, &higher_needle.2, &higher_needle.3, &higher_needle.4);
                 partition_range_vec(&aos, &lower_needle, &higher_needle)
-            });
+            }, BatchSize::SmallInput);
         });
 
         group.bench_with_input("aos_segmented", &size, |b, _size| {
-            b.iter(|| {
-                let (lower_needle, higher_needle) = gen_needles(&mut gen);
+            b.iter_batched(||gen_needles(&mut gen), |(lower_needle, higher_needle)| {
                 let lower_needle = (&lower_needle.0, &lower_needle.1, &lower_needle.2, &lower_needle.3, &lower_needle.4);
                 let higher_needle = (&higher_needle.0, &higher_needle.1, &higher_needle.2, &higher_needle.3, &higher_needle.4);
                 partition_range_vec_segmented(&aos, &lower_needle, &higher_needle)
-            });
+            }, BatchSize::SmallInput);
         });
 
 
         let soa = aos_to_soa(aos);
         group.bench_with_input("soa", &size, |b, _size| {
-            b.iter(|| {
-                let (lower_needle, higher_needle) = gen_needles(&mut gen);
+            b.iter_batched(|| gen_needles(&mut gen), |(lower_needle, higher_needle)| {
                 let lower_needle = (&lower_needle.0, &lower_needle.1, &lower_needle.2, &lower_needle.3, &lower_needle.4);
                 let higher_needle = (&higher_needle.0, &higher_needle.1, &higher_needle.2, &higher_needle.3, &higher_needle.4);
                 partition_range_soa(&soa, &lower_needle, &higher_needle)
-            });
+            }, BatchSize::SmallInput);
         });
     }
 }
