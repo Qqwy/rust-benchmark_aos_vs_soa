955abfce74572f7dea17c794f7fba331a3905618
qqwy <qqwy@gmx.com>
2023-02-24 14:51:31 +0100

    cargo fmt

--- benches/search.rs
+++ benches/search.rs
@@ -1,11 +1,14 @@
-use criterion::{criterion_group, criterion_main, Criterion, BatchSize, Throughput, PlotConfiguration, AxisScale, BenchmarkGroup, measurement::WallTime};
+use criterion::{
+    criterion_group, criterion_main, measurement::WallTime, AxisScale, BatchSize, BenchmarkGroup,
+    Criterion, PlotConfiguration, Throughput,
+};
+use quickcheck::{Arbitrary, Gen};
 use std::time::Duration;
-use quickcheck::{Gen, Arbitrary};
 
 use soa_bench::*;
 
 fn gen_needles<T0, T1, T2, T3, T4>(gen: &mut Gen) -> ((T0, T1, T2, T3, T4), (T0, T1, T2, T3, T4))
-    where
+where
     T0: Ord + Arbitrary,
     T1: Ord + Arbitrary,
     T2: Ord + Arbitrary,
@@ -19,21 +22,41 @@ fn gen_needles<T0, T1, T2, T3, T4>(gen: &mut Gen) -> ((T0, T1, T2, T3, T4), (T0,
     (lower_needle.clone(), higher_needle.clone())
 }
 
-pub fn benchme<T0, T1, T2, T3, T4>(group: &mut BenchmarkGroup<WallTime>, name: &str, gen: &mut Gen, size: usize, fun: impl Fn(&(&T0, &T1, &T2, &T3, &T4), &(&T0, &T1, &T2, &T3, &T4)) -> (usize, usize))
-    where
+pub fn benchme<T0, T1, T2, T3, T4>(
+    group: &mut BenchmarkGroup<WallTime>,
+    name: &str,
+    gen: &mut Gen,
+    size: usize,
+    fun: impl Fn(&(&T0, &T1, &T2, &T3, &T4), &(&T0, &T1, &T2, &T3, &T4)) -> (usize, usize),
+) where
     T0: Ord + Arbitrary,
     T1: Ord + Arbitrary,
     T2: Ord + Arbitrary,
     T3: Ord + Arbitrary,
     T4: Ord + Arbitrary,
-
 {
     group.bench_with_input(name, &size, |b, _size| {
-        b.iter_batched(||gen_needles(gen),|(lower_needle, higher_needle)| {
-            let lower_needle = (&lower_needle.0, &lower_needle.1, &lower_needle.2, &lower_needle.3, &lower_needle.4);
-            let higher_needle = (&higher_needle.0, &higher_needle.1, &higher_needle.2, &higher_needle.3, &higher_needle.4);
-            fun(&lower_needle, &higher_needle)
-        }, BatchSize::SmallInput);
+        b.iter_batched(
+            || gen_needles(gen),
+            |(lower_needle, higher_needle)| {
+                let lower_needle = (
+                    &lower_needle.0,
+                    &lower_needle.1,
+                    &lower_needle.2,
+                    &lower_needle.3,
+                    &lower_needle.4,
+                );
+                let higher_needle = (
+                    &higher_needle.0,
+                    &higher_needle.1,
+                    &higher_needle.2,
+                    &higher_needle.3,
+                    &higher_needle.4,
+                );
+                fun(&lower_needle, &higher_needle)
+            },
+            BatchSize::SmallInput,
+        );
     });
 }
 
@@ -44,30 +67,44 @@ where
     T2: Ord + Arbitrary,
     T3: Ord + Arbitrary,
     T4: Ord + Arbitrary,
-
 {
-    let mut group = c.benchmark_group(format!("search {}", std::any::type_name::<(T0, T1, T2, T3, T4)>()));
-    let plot_config = PlotConfiguration::default()
-        .summary_scale(AxisScale::Logarithmic);
+    let mut group = c.benchmark_group(format!(
+        "search {}",
+        std::any::type_name::<(T0, T1, T2, T3, T4)>()
+    ));
+    let plot_config = PlotConfiguration::default().summary_scale(AxisScale::Logarithmic);
     group.plot_config(plot_config);
     group.warm_up_time(Duration::from_secs(1));
     group.measurement_time(Duration::from_secs(3));
 
-    for size in [0, 1, 2, 4, 8, 16, 32, 64, 256, 1024, 2048, 4096, 8192, 16384, 32768, 65536] {
+    for size in [
+        0, 1, 2, 4, 8, 16, 32, 64, 256, 1024, 2048, 4096, 8192, 16384, 32768, 65536,
+    ] {
         group.throughput(Throughput::Elements(size as u64));
 
         let mut gen = Gen::new(size);
         // Generate an array with exactly `size` random elements
-        let mut aos: Vec<(T0, T1, T2, T3, T4)> = (0..size).map(|_| Arbitrary::arbitrary(&mut gen)).collect();
+        let mut aos: Vec<(T0, T1, T2, T3, T4)> =
+            (0..size).map(|_| Arbitrary::arbitrary(&mut gen)).collect();
         aos.sort_unstable();
 
-        benchme(&mut group, "aos", &mut gen, size, |l, h| partition_range_vec(&aos, l, h));
-        benchme(&mut group, "aos_segmented", &mut gen, size, |l, h| partition_range_vec_segmented(&aos, l, h));
-        benchme(&mut group, "aos_segmented_alt", &mut gen, size, |l, h| partition_range_vec_segmented_alt(&aos, l, h));
+        benchme(&mut group, "aos", &mut gen, size, |l, h| {
+            partition_range_vec(&aos, l, h)
+        });
+        benchme(&mut group, "aos_segmented", &mut gen, size, |l, h| {
+            partition_range_vec_segmented(&aos, l, h)
+        });
+        benchme(&mut group, "aos_segmented_alt", &mut gen, size, |l, h| {
+            partition_range_vec_segmented_alt(&aos, l, h)
+        });
 
         let soa = aos_to_soa(aos);
-        benchme(&mut group, "soa", &mut gen, size, |l, h| partition_range_soa(&soa, l, h));
-        benchme(&mut group, "soa_alt", &mut gen, size, |l, h| partition_range_soa_alt(&soa, l, h));
+        benchme(&mut group, "soa", &mut gen, size, |l, h| {
+            partition_range_soa(&soa, l, h)
+        });
+        benchme(&mut group, "soa_alt", &mut gen, size, |l, h| {
+            partition_range_soa_alt(&soa, l, h)
+        });
     }
 }
 
@@ -78,8 +115,8 @@ type T = u64;
 type R = bool;
 criterion_group!(benches,
                  criterion_benchmark<E, A, V, T, R>,
-                 criterion_benchmark<A,V,E,T,R>,
-                 criterion_benchmark<V,A,E,T,R>,
-                 criterion_benchmark<T,E,A,V,R>
+                 criterion_benchmark<A, V, E, T, R>,
+                 criterion_benchmark<V, A, E, T, R>,
+                 criterion_benchmark<T, E, A, V, R>
 );
 criterion_main!(benches);
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
-pub fn partition_range_vec<T0, T1, T2, T3, T4>(vec: &Vec<(T0, T1, T2, T3, T4)>, lower_needle: &(&T0, &T1, &T2, &T3, &T4), higher_needle: &(&T0, &T1, &T2, &T3, &T4)) -> (usize, usize)
+pub fn partition_range_vec<T0, T1, T2, T3, T4>(
+    vec: &Vec<(T0, T1, T2, T3, T4)>,
+    lower_needle: &(&T0, &T1, &T2, &T3, &T4),
+    higher_needle: &(&T0, &T1, &T2, &T3, &T4),
+) -> (usize, usize)
 where
     T0: Ord,
     T1: Ord,
@@ -9,13 +13,18 @@ where
     let lower = vec[..].partition_point(|(ref e0, ref e1, ref e2, ref e3, ref e4)| {
         (e0, e1, e2, e3, e4).cmp(lower_needle).is_lt()
     });
-    let higher = lower + vec[lower..].partition_point(|(ref e0, ref e1, ref e2, ref e3, ref e4)| {
-        (e0, e1, e2, e3, e4).cmp(higher_needle).is_lt()
-    });
+    let higher = lower
+        + vec[lower..].partition_point(|(ref e0, ref e1, ref e2, ref e3, ref e4)| {
+            (e0, e1, e2, e3, e4).cmp(higher_needle).is_lt()
+        });
     (lower, higher)
 }
 
-pub fn partition_range_vec_segmented<T0, T1, T2, T3, T4>(vec: &Vec<(T0, T1, T2, T3, T4)>, lower_needle: &(&T0, &T1, &T2, &T3, &T4), higher_needle: &(&T0, &T1, &T2, &T3, &T4)) -> (usize, usize)
+pub fn partition_range_vec_segmented<T0, T1, T2, T3, T4>(
+    vec: &Vec<(T0, T1, T2, T3, T4)>,
+    lower_needle: &(&T0, &T1, &T2, &T3, &T4),
+    higher_needle: &(&T0, &T1, &T2, &T3, &T4),
+) -> (usize, usize)
 where
     T0: Ord,
     T1: Ord,
@@ -23,49 +32,46 @@ where
     T3: Ord,
     T4: Ord,
 {
-    let lower = vec[..].partition_point(|(ref e0, _, _, _, _)| {
-        e0.cmp(lower_needle.0).is_lt()
-    });
+    let lower = vec[..].partition_point(|(ref e0, _, _, _, _)| e0.cmp(lower_needle.0).is_lt());
 
-    let higher = lower + vec[lower..].partition_point(|(ref e0, _, _, _, _)| {
-        e0.cmp(higher_needle.0).is_lt()
-    });
+    let higher = lower
+        + vec[lower..].partition_point(|(ref e0, _, _, _, _)| e0.cmp(higher_needle.0).is_lt());
 
-    let lower = lower + vec[lower..higher].partition_point(|(_, ref e1, _, _, _)| {
-        e1.cmp(lower_needle.1).is_lt()
-    });
+    let lower = lower
+        + vec[lower..higher].partition_point(|(_, ref e1, _, _, _)| e1.cmp(lower_needle.1).is_lt());
 
-    let higher = lower + vec[lower..higher].partition_point(|(_, ref e1, _, _, _)| {
-        e1.cmp(higher_needle.1).is_lt()
-    });
+    let higher = lower
+        + vec[lower..higher]
+            .partition_point(|(_, ref e1, _, _, _)| e1.cmp(higher_needle.1).is_lt());
 
-    let lower = lower + vec[lower..higher].partition_point(|(_, _, ref e2, _, _)| {
-        e2.cmp(lower_needle.2).is_lt()
-    });
+    let lower = lower
+        + vec[lower..higher].partition_point(|(_, _, ref e2, _, _)| e2.cmp(lower_needle.2).is_lt());
 
-    let higher = lower + vec[lower..higher].partition_point(|(_, _, ref e2, _, _)| {
-        e2.cmp(higher_needle.2).is_lt()
-    });
+    let higher = lower
+        + vec[lower..higher]
+            .partition_point(|(_, _, ref e2, _, _)| e2.cmp(higher_needle.2).is_lt());
 
-    let lower = lower + vec[lower..higher].partition_point(|(_, _, _, ref e3, _)| {
-        e3.cmp(lower_needle.3).is_lt()
-    });
+    let lower = lower
+        + vec[lower..higher].partition_point(|(_, _, _, ref e3, _)| e3.cmp(lower_needle.3).is_lt());
 
-    let higher = lower + vec[lower..higher].partition_point(|(_, _, _, ref e3, _)| {
-        e3.cmp(higher_needle.3).is_lt()
-    });
+    let higher = lower
+        + vec[lower..higher]
+            .partition_point(|(_, _, _, ref e3, _)| e3.cmp(higher_needle.3).is_lt());
 
-    let lower = lower + vec[lower..higher].partition_point(|(_, _, _, _, ref e4)| {
-        e4.cmp(lower_needle.4).is_lt()
-    });
+    let lower = lower
+        + vec[lower..higher].partition_point(|(_, _, _, _, ref e4)| e4.cmp(lower_needle.4).is_lt());
 
-    let higher = lower + vec[lower..higher].partition_point(|(_, _, _, _, ref e4)| {
-        e4.cmp(higher_needle.4).is_lt()
-    });
+    let higher = lower
+        + vec[lower..higher]
+            .partition_point(|(_, _, _, _, ref e4)| e4.cmp(higher_needle.4).is_lt());
 
     (lower, higher)
 }
-pub fn partition_range_vec_segmented_alt<T0, T1, T2, T3, T4>(vec: &Vec<(T0, T1, T2, T3, T4)>, lower_needle: &(&T0, &T1, &T2, &T3, &T4), higher_needle: &(&T0, &T1, &T2, &T3, &T4)) -> (usize, usize)
+pub fn partition_range_vec_segmented_alt<T0, T1, T2, T3, T4>(
+    vec: &Vec<(T0, T1, T2, T3, T4)>,
+    lower_needle: &(&T0, &T1, &T2, &T3, &T4),
+    higher_needle: &(&T0, &T1, &T2, &T3, &T4),
+) -> (usize, usize)
 where
     T0: Ord,
     T1: Ord,
@@ -73,51 +79,47 @@ where
     T3: Ord,
     T4: Ord,
 {
-    let lower = vec[..].partition_point(|(ref e0, _, _, _, _)| {
-        e0.cmp(lower_needle.0).is_lt()
-    });
+    let lower = vec[..].partition_point(|(ref e0, _, _, _, _)| e0.cmp(lower_needle.0).is_lt());
 
-    let lower = lower + vec[lower..].partition_point(|(_, ref e1, _, _, _)| {
-        e1.cmp(lower_needle.1).is_lt()
-    });
+    let lower =
+        lower + vec[lower..].partition_point(|(_, ref e1, _, _, _)| e1.cmp(lower_needle.1).is_lt());
 
-    let lower = lower + vec[lower..].partition_point(|(_, _, ref e2, _, _)| {
-        e2.cmp(lower_needle.2).is_lt()
-    });
+    let lower =
+        lower + vec[lower..].partition_point(|(_, _, ref e2, _, _)| e2.cmp(lower_needle.2).is_lt());
 
-    let lower = lower + vec[lower..].partition_point(|(_, _, _, ref e3, _)| {
-        e3.cmp(lower_needle.3).is_lt()
-    });
+    let lower =
+        lower + vec[lower..].partition_point(|(_, _, _, ref e3, _)| e3.cmp(lower_needle.3).is_lt());
 
-    let lower = lower + vec[lower..].partition_point(|(_, _, _, _, ref e4)| {
-        e4.cmp(lower_needle.4).is_lt()
-    });
+    let lower =
+        lower + vec[lower..].partition_point(|(_, _, _, _, ref e4)| e4.cmp(lower_needle.4).is_lt());
 
-    let higher = lower + vec[lower..].partition_point(|(ref e0, _, _, _, _)| {
-        e0.cmp(higher_needle.0).is_lt()
-    });
+    let higher = lower
+        + vec[lower..].partition_point(|(ref e0, _, _, _, _)| e0.cmp(higher_needle.0).is_lt());
 
-    let higher = lower + vec[lower..higher].partition_point(|(_, ref e1, _, _, _)| {
-        e1.cmp(higher_needle.1).is_lt()
-    });
+    let higher = lower
+        + vec[lower..higher]
+            .partition_point(|(_, ref e1, _, _, _)| e1.cmp(higher_needle.1).is_lt());
 
-    let higher = lower + vec[lower..higher].partition_point(|(_, _, ref e2, _, _)| {
-        e2.cmp(higher_needle.2).is_lt()
-    });
+    let higher = lower
+        + vec[lower..higher]
+            .partition_point(|(_, _, ref e2, _, _)| e2.cmp(higher_needle.2).is_lt());
 
-    let higher = lower + vec[lower..higher].partition_point(|(_, _, _, ref e3, _)| {
-        e3.cmp(higher_needle.3).is_lt()
-    });
+    let higher = lower
+        + vec[lower..higher]
+            .partition_point(|(_, _, _, ref e3, _)| e3.cmp(higher_needle.3).is_lt());
 
-    let higher = lower + vec[lower..higher].partition_point(|(_, _, _, _, ref e4)| {
-        e4.cmp(higher_needle.4).is_lt()
-    });
+    let higher = lower
+        + vec[lower..higher]
+            .partition_point(|(_, _, _, _, ref e4)| e4.cmp(higher_needle.4).is_lt());
 
     (lower, higher)
 }
 
-
-pub fn partition_range_soa<T0, T1, T2, T3, T4>(soa: &(Vec<T0>, Vec<T1>, Vec<T2>, Vec<T3>, Vec<T4>), lower_needle: &(&T0, &T1, &T2, &T3, &T4), higher_needle: &(&T0, &T1, &T2, &T3, &T4)) -> (usize, usize)
+pub fn partition_range_soa<T0, T1, T2, T3, T4>(
+    soa: &(Vec<T0>, Vec<T1>, Vec<T2>, Vec<T3>, Vec<T4>),
+    lower_needle: &(&T0, &T1, &T2, &T3, &T4),
+    higher_needle: &(&T0, &T1, &T2, &T3, &T4),
+) -> (usize, usize)
 where
     T0: Ord,
     T1: Ord,
@@ -125,51 +127,34 @@ where
     T3: Ord,
     T4: Ord,
 {
-    let lower = soa.0[..].partition_point(|e0| {
-        e0.cmp(lower_needle.0).is_lt()
-    });
+    let lower = soa.0[..].partition_point(|e0| e0.cmp(lower_needle.0).is_lt());
 
-    let higher = lower + soa.0[lower..].partition_point(|e0| {
-        e0.cmp(higher_needle.0).is_lt()
-    });
+    let higher = lower + soa.0[lower..].partition_point(|e0| e0.cmp(higher_needle.0).is_lt());
 
+    let lower = lower + soa.1[lower..higher].partition_point(|e1| e1.cmp(lower_needle.1).is_lt());
 
-    let lower = lower + soa.1[lower..higher].partition_point(|e1| {
-        e1.cmp(lower_needle.1).is_lt()
-    });
+    let higher = lower + soa.1[lower..higher].partition_point(|e1| e1.cmp(higher_needle.1).is_lt());
 
-    let higher = lower + soa.1[lower..higher].partition_point(|e1| {
-        e1.cmp(higher_needle.1).is_lt()
-    });
+    let lower = lower + soa.2[lower..higher].partition_point(|e2| e2.cmp(lower_needle.2).is_lt());
 
-    let lower = lower + soa.2[lower..higher].partition_point(|e2| {
-        e2.cmp(lower_needle.2).is_lt()
-    });
+    let higher = lower + soa.2[lower..higher].partition_point(|e2| e2.cmp(higher_needle.2).is_lt());
 
-    let higher = lower + soa.2[lower..higher].partition_point(|e2| {
-        e2.cmp(higher_needle.2).is_lt()
-    });
+    let lower = lower + soa.3[lower..higher].partition_point(|e3| e3.cmp(lower_needle.3).is_lt());
 
-    let lower = lower + soa.3[lower..higher].partition_point(|e3| {
-        e3.cmp(lower_needle.3).is_lt()
-    });
+    let higher = lower + soa.3[lower..higher].partition_point(|e3| e3.cmp(higher_needle.3).is_lt());
 
-    let higher = lower + soa.3[lower..higher].partition_point(|e3| {
-        e3.cmp(higher_needle.3).is_lt()
-    });
+    let lower = lower + soa.4[lower..higher].partition_point(|e4| e4.cmp(lower_needle.4).is_lt());
 
-    let lower = lower + soa.4[lower..higher].partition_point(|e4| {
-        e4.cmp(lower_needle.4).is_lt()
-    });
-
-    let higher = lower + soa.4[lower..higher].partition_point(|e4| {
-        e4.cmp(higher_needle.4).is_lt()
-    });
+    let higher = lower + soa.4[lower..higher].partition_point(|e4| e4.cmp(higher_needle.4).is_lt());
 
     (lower, higher)
 }
 
-pub fn partition_range_soa_alt<T0, T1, T2, T3, T4>(soa: &(Vec<T0>, Vec<T1>, Vec<T2>, Vec<T3>, Vec<T4>), lower_needle: &(&T0, &T1, &T2, &T3, &T4), higher_needle: &(&T0, &T1, &T2, &T3, &T4)) -> (usize, usize)
+pub fn partition_range_soa_alt<T0, T1, T2, T3, T4>(
+    soa: &(Vec<T0>, Vec<T1>, Vec<T2>, Vec<T3>, Vec<T4>),
+    lower_needle: &(&T0, &T1, &T2, &T3, &T4),
+    higher_needle: &(&T0, &T1, &T2, &T3, &T4),
+) -> (usize, usize)
 where
     T0: Ord,
     T1: Ord,
@@ -177,54 +162,40 @@ where
     T3: Ord,
     T4: Ord,
 {
-    let lower = soa.0[..].partition_point(|e0| {
-        e0.cmp(lower_needle.0).is_lt()
-    });
+    let lower = soa.0[..].partition_point(|e0| e0.cmp(lower_needle.0).is_lt());
 
-    let lower = lower + soa.1[lower..].partition_point(|e1| {
-        e1.cmp(lower_needle.1).is_lt()
-    });
+    let lower = lower + soa.1[lower..].partition_point(|e1| e1.cmp(lower_needle.1).is_lt());
 
-    let lower = lower + soa.2[lower..].partition_point(|e2| {
-        e2.cmp(lower_needle.2).is_lt()
-    });
+    let lower = lower + soa.2[lower..].partition_point(|e2| e2.cmp(lower_needle.2).is_lt());
 
-    let lower = lower + soa.3[lower..].partition_point(|e3| {
-        e3.cmp(lower_needle.3).is_lt()
-    });
+    let lower = lower + soa.3[lower..].partition_point(|e3| e3.cmp(lower_needle.3).is_lt());
 
-    let lower = lower + soa.4[lower..].partition_point(|e4| {
-        e4.cmp(lower_needle.4).is_lt()
-    });
+    let lower = lower + soa.4[lower..].partition_point(|e4| e4.cmp(lower_needle.4).is_lt());
 
-    let higher = lower + soa.0[lower..].partition_point(|e0| {
-        e0.cmp(higher_needle.0).is_lt()
-    });
+    let higher = lower + soa.0[lower..].partition_point(|e0| e0.cmp(higher_needle.0).is_lt());
 
-    let higher = lower + soa.1[lower..higher].partition_point(|e1| {
-        e1.cmp(higher_needle.1).is_lt()
-    });
+    let higher = lower + soa.1[lower..higher].partition_point(|e1| e1.cmp(higher_needle.1).is_lt());
 
-    let higher = lower + soa.2[lower..higher].partition_point(|e2| {
-        e2.cmp(higher_needle.2).is_lt()
-    });
+    let higher = lower + soa.2[lower..higher].partition_point(|e2| e2.cmp(higher_needle.2).is_lt());
 
+    let higher = lower + soa.3[lower..higher].partition_point(|e3| e3.cmp(higher_needle.3).is_lt());
 
-    let higher = lower + soa.3[lower..higher].partition_point(|e3| {
-        e3.cmp(higher_needle.3).is_lt()
-    });
-
-
-    let higher = lower + soa.4[lower..higher].partition_point(|e4| {
-        e4.cmp(higher_needle.4).is_lt()
-    });
+    let higher = lower + soa.4[lower..higher].partition_point(|e4| e4.cmp(higher_needle.4).is_lt());
 
     (lower, higher)
 }
 
-pub fn aos_to_soa<T0, T1, T2, T3, T4>(aos: Vec<(T0, T1, T2, T3, T4)>) -> (Vec<T0>, Vec<T1>, Vec<T2>, Vec<T3>, Vec<T4>) {
+pub fn aos_to_soa<T0, T1, T2, T3, T4>(
+    aos: Vec<(T0, T1, T2, T3, T4)>,
+) -> (Vec<T0>, Vec<T1>, Vec<T2>, Vec<T3>, Vec<T4>) {
     let len = aos.len();
-    let mut soa = (Vec::with_capacity(len), Vec::with_capacity(len), Vec::with_capacity(len), Vec::with_capacity(len), Vec::with_capacity(len));
+    let mut soa = (
+        Vec::with_capacity(len),
+        Vec::with_capacity(len),
+        Vec::with_capacity(len),
+        Vec::with_capacity(len),
+        Vec::with_capacity(len),
+    );
 
     for (e0, e1, e2, e3, e4) in aos {
         soa.0.push(e0);
